4fdc312d886229a3a5aa9ee0ba12d05e0b7a2259
Maruthi Pothuganti <maruthicharypothuganti@gmail.com>
2022-07-18 14:21:54 +0530

    [feat]- Address update and delete through buttons

--- src/Components/Address/Address.jsx
+++ src/Components/Address/Address.jsx
@@ -5,7 +5,7 @@ import { useAddress } from "../../Context/AddressContext";
 import { AddressModal } from "../AddressModal/AddressModal";
 
 export const Address = () => {
-  const { addresses } = useAddress();
+  const { addresses, removeAddress } = useAddress();
   const [isAddressModalOpen, setIsAddressModal] = useState(false);
 
   const initialAddressState = {
@@ -43,11 +43,7 @@ export const Address = () => {
       </div>
       <div className="flex-column-center">
         {addresses.map((address) => (
-          <div
-            className="addressCard"
-            key={address._id}
-            onClick={() => updateHandler(address)}
-          >
+          <div className="addressCard" key={address._id}>
             <h2>
               {address.fullName}{" "}
               {address.default ? (
@@ -60,6 +56,21 @@ export const Address = () => {
               {address.houseNo}, {address.city}, {address.state},{" "}
               {address.country}, {address.mobile}, {address.ZIP}.
             </h3>
+            <div className="addressCTA">
+              <i
+                className="fas fa-edit fa-xl"
+                title="Edit"
+                onClick={() => updateHandler(address)}
+              ></i>
+              <i
+                className="fas fa-trash-alt fa-xl"
+                title="Delete"
+                onClick={() => {
+                  removeAddress(address);
+                  toast.info("Address Removed");
+                }}
+              ></i>
+            </div>
           </div>
         ))}
       </div>
